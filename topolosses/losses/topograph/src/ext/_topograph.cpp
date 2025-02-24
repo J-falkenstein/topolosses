799b9e9fa69718ed086d756d3732a47351ec5d01
J-falkenstein <janekfalkenstein@MacBook-Air-von-Janek.local>
2025-02-24 15:31:12 +0100

    cleaning cpp file

--- topolosses/losses/topograph/src/ext/_topograph.cpp
+++ topolosses/losses/topograph/src/ext/_topograph.cpp
@@ -7,32 +7,6 @@
 namespace py = pybind11;
 using namespace pybind11::literals;
 
-// struct S {
-//     int x;
-//     explicit S(int x): x{x} {
-//         std::cout << "construct S(" << x << ")" << std::endl;
-//     }
-//     S(const S &s): x{s.x} {
-//         std::cout << "copy S(const S &s)" << std::endl;
-//     }
-// };
-
-// S make_value(int x) {
-//     return S(x);
-// }
-
-// S make_double_value(int x) {
-//     S s = make_value(x);
-//     s.x = 2*x;
-//     return s;
-// }
-
-// std::tuple<int, int> copy_test() {
-//     S s1 = make_double_value(21);
-//     S s2 = make_double_value(23);
-//     return std::make_tuple(s1.x, s2.x);
-// }
-
 PYBIND11_MODULE(Topograph, m) {
     m.doc() = "Highly Efficient C++ Topograph implementation"; // optional module docstring
 
