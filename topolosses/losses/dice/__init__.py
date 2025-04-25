267a7cc3e19260349cc4272f14cfb1686849691c
J-falkenstein <j.falkenstein@tum.de>
2025-04-25 15:13:03 +0200

    improving sphinx doc

--- topolosses/losses/dice/__init__.py
+++ topolosses/losses/dice/__init__.py
@@ -5,5 +5,9 @@
 #     generate_pin_number,
 #     generate_credit_card_number,
 # )
-# TODO does it make sense to make something like this to have it easier to import the class? 
+# TODO does it make sense to make something like this to have it easier to import the class?
 # the above is used to make it easier to import functions
+
+from .src.dice_loss import DiceLoss
+
+__all__ = ["DiceLoss"]
